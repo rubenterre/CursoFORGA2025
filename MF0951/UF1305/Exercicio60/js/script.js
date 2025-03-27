ff07a3dc24f6ce550eaff96c95f44e6f9086f5e8
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2025-03-27 11:26:58 +0100

    Update Exercicio60

--- MF0951/UF1305/Exercicio60/js/script.js
+++ MF0951/UF1305/Exercicio60/js/script.js
@@ -1,9 +1,32 @@
+
+//Navbar BulmaCSS Mobile
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+  
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+  
+      });
+    });
+  
+  });
+
+
+
 // API OpenWeatherMaps
 
 const API_KEY = "3e867330616c39fa60d18a1af5d82f16"
-
-/* const lat = "42.84"
-const lon = "-8.57" */
 
 async function obterDatosTempo(lat, lon) {
     const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=gl`
