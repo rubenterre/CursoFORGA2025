50f516ba4f5f7685b8c29f2808bc6dde0c369845
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2025-04-22 12:15:42 +0200

    Update Exercicio70

--- MF0951/UF1306/Exercicio70/js/script.js
+++ MF0951/UF1306/Exercicio70/js/script.js
@@ -1,1 +1,12 @@
 var rellax = new Rellax('.rellax');
+
+
+// Función para alternar el menú de navegación
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
